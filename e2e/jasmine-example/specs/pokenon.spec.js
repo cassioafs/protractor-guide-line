47346162e9737617b0c53a60f79a044947a0a50f
Cássio Alves <cassiocafs@gmail.com>
2019-09-17 11:11:55 -0300

    update

--- e2e/jasmine-example/specs/pokenon.spec.js
+++ e2e/jasmine-example/specs/pokenon.spec.js
@@ -1,6 +1,5 @@
 'use strict'
 const ListagemPage = require('../../pages/listagem.po.js');
-const POKEMON = require('../../data/db.js');
 
 describe('Listagem de pokémons', ()=> {
   const listagemPage = new ListagemPage();
@@ -8,14 +7,5 @@ describe('Listagem de pokémons', ()=> {
     listagemPage.visit();
     listagemPage.pesquisarPokemon('mew');
     expect(listagemPage.resultados.count()).toEqual(2);
-  });
-});  
-
-describe('Inclusão de pokémons', ()=> {
-  const listagemPage = new ListagemPage();
-  it('Deve incluir um pokémon', ()=>{
-    listagemPage.visit();
-    listagemPage.incluirPokemon(POKEMON.new_pokemon);
-    expect(listagemPage.resultados.count()).toEqual(6);
   });
 });  
--- e2e/pages/listagem.po.js
+++ e2e/pages/listagem.po.js
@@ -4,17 +4,6 @@ class ListagemPage{
   constructor(){
     this.filtro = element(by.dataTest('filtro'));
     this.resultados = element.all(by.repeater('item in PokemonsController.pokemons'));;
-    this.btnAdicionar = element(by.linkText('Adicionar'));
-    this.numero = element(by.model('PokemonsController.pokemon.id')); 
-    this.nome = element(by.model('PokemonsController.pokemon.name')); 
-    this.ataque = element(by.model('PokemonsController.pokemon.attack')); 
-    this.defesa = element(by.model('PokemonsController.pokemon.defense')); 
-    this.hp = element(by.model('PokemonsController.pokemon.hp')); 
-
-    this.btnSalvar = element(by.buttonText('Salvar'));
-
-    this.modalCampos = element(by.id('myModal'));
-
   }
 
   visit(){
@@ -23,21 +12,6 @@ class ListagemPage{
 
   pesquisarPokemon(nome){
     return this.filtro.sendKeys(nome);
-  }
-
-  incluirPokemon(pokemon){
-    this.btnAdicionar.click();
-
-    const EC = protractor.ExpectedConditions;
-    browser.wait(EC.visibilityOf(this.modalCampos));
-
-    this.numero.sendKeys(pokemon.numero);
-    this.nome.sendKeys(pokemon.nome);
-    this.ataque.sendKeys(pokemon.ataque);
-    this.defesa.sendKeys(pokemon.defesa);
-    this.hp.sendKeys(pokemon.hp);
-    this.btnSalvar.click();
-
   }
 
 }
